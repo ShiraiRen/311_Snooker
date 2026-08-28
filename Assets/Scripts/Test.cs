8badd6d899e6e180b531f57be6bec27106f1ffbe
ShiraiRen <shorai254@gmail.com>
2026-08-28 16:13:55 +0700

    Table Commit

---
+++ Assets/Scripts/Test.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    void Awake()
+
+    {
+        Debug.Log("Awake");
+
+    }
+
+   
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log("Start");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
